a46d4524c6f388767ab1ce967627214a9112dbaa
galih <samudragalih94@gmail.com>
2023-10-24 15:36:11 +0700

    feat(Tests): create test memory tracking

---
+++ PokemonGameNetwork/PokemonGameNetworkTests/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,18 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  PokemonGameNetworkTests
+//
+//  Created by Galih Samudra on 24/10/23.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have beed deallocated, potential memory leak", file: file, line: line)
+        }
+    }
+    
+}
