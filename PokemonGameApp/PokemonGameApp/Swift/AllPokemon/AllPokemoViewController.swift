55236e5245697dc559b34b057fbeaa52b8c26d0e
galih <samudragalih94@gmail.com>
2023-10-24 18:42:40 +0700

    feat(AllPokemoViewController): create `AllPokemoViewController`

---
+++ PokemonGameApp/PokemonGameApp/Swift/AllPokemon/AllPokemoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AllPokemoViewController.swift
+//  PokemonGameApp
+//
+//  Created by Galih Samudra on 24/10/23.
+//
+
+import Foundation
+import UIKit
+
+final class AllPokemoViewController: UICollectionViewController {
+    
+}
