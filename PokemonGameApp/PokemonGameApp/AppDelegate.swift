b468a43e071aeaae03d1a4ad6472be696e909e1c
galih <samudragalih94@gmail.com>
2023-10-24 16:51:57 +0700

    feat(App): create PokemonGameApp

---
+++ PokemonGameApp/PokemonGameApp/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  PokemonGameApp
+//
+//  Created by Galih Samudra on 24/10/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ PokemonGameApp/PokemonGameAppTests/PokemonGameAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonGameAppTests.swift
+//  PokemonGameAppTests
+//
+//  Created by Galih Samudra on 24/10/23.
+//
+
+import XCTest
+@testable import PokemonGameApp
+
+final class PokemonGameAppTests: XCTestCase {
+
+    func testExample() throws {
+    }
+
+}
